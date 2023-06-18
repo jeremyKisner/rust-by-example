41618b8682944345eaf30a4b94cbb2660ae8a9ed
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-06-17 23:10:21 -0400

    Update main.rs
    add loop and break when finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,36 @@
 use std::io::stdin;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() {
     println!("guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("please input a guess.");
 
-    let mut guess = String::new();
+    loop {
+        println!("please input a guess.");
+        let mut guess = String::new();
 
-    stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-
-    println!("You guessed: {guess}");
+        stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please input a number!");
+    
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 
     println!("The number was {secret_number}!");
 }
